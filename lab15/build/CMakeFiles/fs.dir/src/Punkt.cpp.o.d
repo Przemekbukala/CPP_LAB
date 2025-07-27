b5470aaeae5d152a2714c198de1c76ad49e632e3
Przemysław Bukala <bukalaprzemyslaw@gmail.com>
2025-07-27 18:16:09 +0200

    Added .gitignore file

--- lab15/build/CMakeFiles/fs.dir/src/Punkt.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/fs.dir/src/Punkt.cpp.o: \
- /home/przemyslaw-bukala/Semestr_4/CPP/semestr_4/lab15/src/Punkt.cpp \
- /usr/include/stdc-predef.h \
- /home/przemyslaw-bukala/Semestr_4/CPP/semestr_4/lab15/include/Punkt.h
